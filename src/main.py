51d25ee513195461cf8f000b94e9d93f552cf773
youcefaidounmi <youcef.aidoun.mi@gmail.com>
2022-06-25 00:01:10 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication
+
 # load ui
-from PySide6.QtCore import QFile, QIODevice
 from PyQt6 import uic
+
 # show incons => cmd: pyside6-rcc icons.qrc -o icons.py
 import icons
-import os
-import stat
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
