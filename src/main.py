917f3555b78439a29c38fba3c81293a091b2f9af
youcefaidounmi <youcef.aidoun.mi@gmail.com>
2022-06-25 09:51:41 +0100

    save file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+# src for examples : https://programtalk.com/python-more-examples/
+
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 
 # load ui
 from PyQt6 import uic
 
-# show incons => cmd: pyside6-rcc icons.qrc -o icons.py
+# show incons
+""" cmd: pyside6-rcc icons.qrc -o icons.py 
+    main.py -> import icons """
 import icons
 
 
@@ -12,11 +16,37 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # connect to ui => src: https://doc.qt.io/qtforpython/tutorials/basictutorial/uifiles.html
+        # load ui
+        """ src: https://doc.qt.io/qtforpython/tutorials/basictutorial/uifiles.html """
         self.ui = uic.loadUi("ui/uimain.ui")
 
-        # show ui =======================
+        # show ui
         self.ui.show()
+
+        # save File
+        self.ui.actionSave.triggered.connect(self.saveFile)
+
+    # save File
+    def saveFile(self):
+        """ src: https://doc-snapshots.qt.io/qt6-dev/qfiledialog.html#getSaveFileName """
+        try:
+            fileName = QFileDialog.getSaveFileName(self, "Save file")
+            """ fileName -> [path, ext] """
+            if fileName[0]:
+                """ w -> write"""
+                file = open(fileName[0], "w")
+                text = self.ui.textEdit.toPlainText()
+                file.write(text)
+                file.close()
+            QMessageBox.about(self, "Save file", "save done. \n \f path: " +fileName[0])
+        except Exception as error:
+            print(error)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
